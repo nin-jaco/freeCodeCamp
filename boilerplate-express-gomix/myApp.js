2c3351d1dd104e5e1c76b51c2605fcd622514f5c
jaco <jaco.kleynhans@icloud.com>
2020-07-28 17:59:41 +0200

    Starting Mongoose and MongoDb

--- boilerplate-express-gomix/myApp.js
+++ boilerplate-express-gomix/myApp.js
@@ -1,5 +1,6 @@
 
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
 
 // --> 7)  Mount the Logger middleware here
@@ -33,28 +34,61 @@ app.get("/json", (req,res) => {
 });
 
 /** 6) Use the .env file to configure the app */
- 
- 
+process.env.MESSAGE_STYLE="uppercase"
+app.get("/json", (req,res) => {
+    if(process.env.MESSAGE_STYLE == "uppercase")
+    {
+        res.json({"message" : "HELLO JSON"});
+    }
+    else{
+        res.json({"message" : "Hello json"});
+    }
+});
+
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
+app.use(function logger(req, res, next){
+    console.log(req.method + " " + req.path + " - "+ req.ip);
+    next();
+});
 
 /** 8) Chaining middleware. A Time server */
-
+function getTheTime(req, res, next)
+{
+    return new Date().toString();
+}
+app.get('/now', function(req, res, next) {
+    req.time = getTheTime();  // Hypothetical synchronous operation
+    next();
+  }, function(req, res) {
+    res.json({"time" : req.time});
+  });
 
 /** 9)  Get input from client - Route parameters */
-
+app.get("/:word/echo", (req,res) => {
+    var word = req.params.word;
+    res.json({"echo" : word});
+});
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
+app.get("/name", (req,res) => {
+    var firstname = req.query.first;
+    var lastname = req.query.last;
+    res.json({"name" : `${firstname} ${lastname}`});
+});
 
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
-
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 /** 12) Get data form POST  */
-
+app.post("/name", (req,res) => {
+    var string = req.body.first + " " + req.body.last;
+    res.json({"name" : string});
+});
 
 
 // This would be part of the basic setup of an Express app
